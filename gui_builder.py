0b0afd94e23a196cc2b6731b25d274dc3ca36ef0
prat-bphc52 <hp.agarwal1@gmail.com>
2019-11-19 06:02:55 +0530

    Fix bug causing logo to update after selecting team

--- gui_builder.py
+++ gui_builder.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 
+teamNames = ['Mumbai Indians','Kolkata Knight Riders','Royal Challengers Bangalore','Deccan Chargers','Chennai Super Kings','Rajasthan Royals','Delhi Daredevils','Gujarat Lions','Kings XI Punjab','Sunrisers Hyderabad','Rising Pune Supergiants','Kochi Tuskers Kerala','Pune Warriors']
 imgAddressPrefix = r"C:\Users\pratd\Documents\COURSES\3-1\CS F407 AI\Cricket_Match_Predictor\assets"
 imageTeams = [r"\mi.png",r"\kkr.png",r"\rcb.png",r"\dc.png",r"\csk.png",r"\rr.png",r"\dd.png",r"\gl.png",r"\kxip.png",r"\srh.png",r"\rps.png",r"\ktk.png",r"\pw.png"]
 
@@ -11,9 +12,10 @@ master.title('IPL Cricket Match Predictor')
 l1 = Label(master, text = "Team A", font=("Helvetica", 14, 'bold')) 
 l2 = Label(master, text = "Team B", font=("Helvetica", 14, 'bold')) 
 l3 = Label(master, text = "Venue") 
-l4 = Label(master, text = "Match Date") 
-l5 = Label(master, text = "Toss Winner") 
-l6 = Label(master, text = "VS", font=("Helvetica", 12, 'italic')) 
+l4 = Label(master, text = "City") 
+l5 = Label(master, text = "Match Date") 
+l6 = Label(master, text = "Toss Winner") 
+labelVS = Label(master, text = "VS", font=("Helvetica", 12, 'italic')) 
   
 # grid method to arrange labels in respective 
 # rows and columns as specified 
@@ -22,7 +24,8 @@ l2.grid(row = 3, column = 4, columnspan = 3, pady = 10)
 l3.grid(row = 5, column = 0, sticky = W, pady = 10, padx = 10) 
 l4.grid(row = 6, column = 0, sticky = W, pady = 10, padx = 10) 
 l5.grid(row = 7, column = 0, sticky = W, pady = 10, padx = 10) 
-l6.grid(row = 1, column = 3) 
+l6.grid(row = 8, column = 0, sticky = W, pady = 10, padx = 10) 
+labelVS.grid(row = 1, column = 3) 
   
 #list of Teams in IPL
 tkvar = StringVar(master)
@@ -39,29 +42,34 @@ imgA = PhotoImage(file = imgAddressPrefix+imageTeams[0])
 img1A = imgA.subsample(2, 2) 
   
 # setting image with the help of label 
-teamAlogo = Label(master, image = img1A).grid(row = 0, column = 0, 
+teamAlogo = Label(master, image = img1A)
+teamAlogo.grid(row = 0, column = 0, 
        columnspan = 3, rowspan = 3, padx = 5, pady = 20) 
 
 imgB = PhotoImage(file = imgAddressPrefix+imageTeams[4])
 img1B = imgB.subsample(2, 2) 
   
 # setting image with the help of label 
-teamBlogo = Label(master, image = img1B).grid(row = 0, column = 4, 
+teamBlogo = Label(master, image = img1B)
+teamBlogo.grid(row = 0, column = 4, 
        columnspan = 3, rowspan = 3, padx = 5, pady = 20) 
 
 def teamASelected(event):
-    # imgtemp =  PhotoImage(file = imgAddressPrefix+imageTeams[8])
-    # teamAlogo.configure(image = imgtemp)
-    print('here A',event)
+    imgtemp =  PhotoImage(file = imgAddressPrefix+imageTeams[teamNames.index(event)])
+    imgtemp = imgtemp.subsample(2, 2) 
+    teamAlogo.configure(image = imgtemp)
+    teamAlogo.image = imgtemp
     pass
 
 def teamBSelected(event):
-    # imgtemp =  PhotoImage(file = imgAddressPrefix+imageTeams[8])
-    # teamBlogo.configure(image = imgtemp)
-    print('hereB',event)
+    imgtemp =  PhotoImage(file = imgAddressPrefix+imageTeams[teamNames.index(event)])
+    imgtemp = imgtemp.subsample(2, 2) 
+    teamBlogo.configure(image = imgtemp)
+    teamBlogo.image = imgtemp
     pass
 
 popupTeamA = OptionMenu(master, tkvar, *choices, command = teamASelected)
+popupTeamA.bind
 popupTeamB = OptionMenu(master, tkvar2, *choices2, command = teamBSelected)
 
 # this will arrange entry widgets 
